a61c0a81f492985a42dff4fce3ca58317a19a14f
Adam Konber <konber3@gmail.com>
2024-10-02 10:30:22 -0600

    changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json()); // Enable JSON body to be parsed
 
 app.use(cors()); // Allow all CORS requests
 
-let users = []; // This is your fake database
+app.locals.users = []; // This is your fake database
 
 app.get('/', (request, response) => {
   response.status(200).json({hello: 'world'});
@@ -15,11 +15,23 @@ app.get('/', (request, response) => {
 
 // Your new endpoints (routes) can go here **************
 
-
-
+app.get('/api/v1/users', (req, res)=> {
+  const users = app.locals.users
+  if (users.length === 0) {
+      return res.status(501).send({message: "No user found"})
+  }
+  res.status(200).json(users)
+})
+app.post('/api/v1/users', (req,res) => {
+  if (!req.body.name || !req.body.email) {
+      res.status(400).send({message: "Fix your body"})
+  }
+  app.locals.users.push({...req.body, id:Date.now()})
+  res.status(201).send(app.locals.users)
+})
 
 // ******************************************************
 
 app.listen(app.get('port'), () => {
-  console.log(`Auth0 practice BE running on http://localhost:${app.get('port')}`);
+  console.log(`Getting money on http://localhost:${app.get('port')}`);
 });
